c3b473a2013e0f23ab8bd5256787b379629f78b8
Nahov91 <35198701+Nahov91@users.noreply.github.com>
2018-10-29 23:27:40 +0100

    Fix: style Search section as spec

--- src/Components/Sessions.js
+++ src/Components/Sessions.js
@@ -4,18 +4,25 @@ import Post from "./Post"
 export default class Sessions extends Component {
     render() {
         return (
-            <>
+            <section id="sessions">
                 <h1 className="title">SESSIONS</h1>
-                <form action="" id="search-form">
-                    <input type="search" className="search" placeholder="Search name, keyword" />
-                    <select name="Topic" id="topic">
-                        <option value="first">First</option>
-                        <option value="second">Second</option>
-                    </select>
-                    <input type="date" id="date" />
-                </form>
+                <div className="form-wrapper">
+                    <form action="" id="search-form">
+                        <input type="search" className="search" placeholder="Search name, keyword..." />
+                        <select  className="topic" name="Topic">
+                            <option value="Topic" disabled selected value>Topic</option>
+                            <option value="going digital">Going digital</option>
+                            <option value="second">Second</option>
+                        </select>
+                        <select className="date" name="Day">
+                            <option value="Day" disabled selected value>Day</option>
+                            <option value="Day 1">Day 1</option>
+                            <option value="Day 2">Day 2</option>
+                        </select>
+                    </form>
+                </div>
                 <Post/>
-            </>
+            </section>
         )
     }
 }
