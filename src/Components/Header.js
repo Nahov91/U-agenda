ae53b09dd6da5735992f62b0417d5fa273559e09
Nahov91 <35198701+Nahov91@users.noreply.github.com>
2018-10-29 12:24:26 +0100

    Fix: header now looks as spec

--- src/Components/Header.js
+++ src/Components/Header.js
@@ -3,14 +3,22 @@ import React, { Component } from 'react'
 export default class Header extends Component {
   render() {
     return (
-      <div>
-        <header className="header">
-            <img className="headerImage" src="https://res.cloudinary.com/nahov91/image/upload/v1540555423/Circle__transparent.png" alt=""/>
-            <img className="headerImage" src="https://res.cloudinary.com/nahov91/image/upload/v1540555423/Circle__transparent.png" alt=""/>
-            <img className="headerImage" src="https://res.cloudinary.com/nahov91/image/upload/v1540555423/Circle__transparent.png" alt=""/>
-            <img className="headerImage" src="https://res.cloudinary.com/nahov91/image/upload/v1540555423/Circle__transparent.png" alt=""/>
+        <header id="header">
+        <button className="headerLogo">
+            <img src="https://res.cloudinary.com/nahov91/image/upload/v1540807689/3_black.png" alt=""/>
+            </button>
+        <nav>
+          <button className="headerImage">
+            <img src="https://res.cloudinary.com/nahov91/image/upload/v1540807689/Shape.png" alt=""/>
+            </button>
+            <button className="headerImage">
+            <img src="https://res.cloudinary.com/nahov91/image/upload/v1540807689/profile_1335.png" alt=""/>
+            </button>
+            <button className="headerImage">
+            <img src="https://res.cloudinary.com/nahov91/image/upload/v1540807776/Shape_1.png" alt=""/>
+            </button>
+            </nav>
         </header>
-      </div>
     )
   }
 }
