4e4088ea4fadcdb7c2a6cfc63349f23fc0219cb9
Nahov91 <35198701+Nahov91@users.noreply.github.com>
2018-10-31 00:32:54 +0100

    Feat: add post location and date icons

--- src/Components/Post.js
+++ src/Components/Post.js
@@ -17,8 +17,20 @@ export default class Post extends Component {
         <li className="presentation-item" key={presentation._id}>
           <section className="post">
             <h5 className="post-tag" style={{ background: presentation.stage.color}} > {presentation.stage.name} </h5>
-            <p className="post-location"> {presentation.stage.room} </p>
-            <date className="post-date"> Day {presentation.day} {presentation.dateFrom} </date>
+            <div className="post-location">
+              <img
+                src="https://res.cloudinary.com/nahov91/image/upload/v1540942137/Icon_24px_1.png"
+                alt="date"
+              />
+              <p> {presentation.stage.room} </p>
+            </div>
+            <div className="post-date">
+            <img
+                src="https://res.cloudinary.com/nahov91/image/upload/v1540826910/Icon_24px.png"
+                alt="date"
+              />
+              <time> Day {presentation.day} {presentation.dateFrom} </time>
+            </div>
             <h3 className="post-title"> {presentation.title} </h3>
             <p className="post-description"> {presentation.abstract.substring(0,220)} <a href="google.com" className="more-link">...More</a> </p>
             <Creator />
