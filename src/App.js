a18041cf34ebe7927a0774b3dd4b9479199afa0a
Nahov91 <35198701+Nahov91@users.noreply.github.com>
2018-10-30 09:27:55 +0100

    Feat: fetch API data, set in local state

--- src/App.js
+++ src/App.js
@@ -4,6 +4,26 @@ import Header from './Components/Header';
 import Main from './Components/Main';
 
 class App extends Component {
+  constructor(props) {
+    super(props);
+
+    this.state = {
+      allData: {},
+    }
+  }
+
+
+  componentDidMount() {
+    fetch('https://unleash.site/api/agendas/list?event=5b86aa9be9bef44a7491e80f')
+    .then(response => response.json())
+    .then(data =>
+      this.setState({
+        allData: data,
+      }))
+      .catch(err => alert("Error loading data from the server"));
+  }
+  
+
   render() {
     return (
       <div className="App">
